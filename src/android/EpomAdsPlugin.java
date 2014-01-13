0fb4ab65312dabb6661b1b173e822580e1df37b2
Jef <jef@yabos.local>
2014-01-13 13:22:21 +0100

    Android skeleton

---
+++ src/android/EpomAdsPlugin.java
@@ -0,0 +1,12 @@
+package com.cahri.phonegap.epom;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.json.JSONArray;
+
+public class EpomAdsPlugin extends CordovaPlugin {
+        @Override
+        public boolean execute(String action, JSONArray args, CallbackContext callback) {
+        	// TODO
+        }
+}
